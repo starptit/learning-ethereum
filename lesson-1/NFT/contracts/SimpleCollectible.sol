b81ed600dd3413b5ab7ae067da7703f7aa580075
Shaw Vu <genesis@Shaws-iMac.local>
2021-10-01 00:11:08 +0700

    Add sample collectible

--- lesson-1/NFT/contracts/SimpleCollectible.sol
+++ lesson-1/NFT/contracts/SimpleCollectible.sol
@@ -1,2 +1,26 @@
 
 // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+
+
+contract SimpleCollectible is ERC721 {
+    uint256 public tokenCounter;
+
+    constructor () public ERC721 ("SwiftyVN", "Swifty") {
+        tokenCounter = 0;    
+    }
+
+    function createCollectible(string memory tokenURI) public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
+
+}
---
+++ lesson-1/NFT/scripts/helpful_scripts.py
@@ -0,0 +1,15 @@
+from brownie import accounts, network, config, SimpleCollectible
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache", "mainnet-fork", "binance-fork", "matic-fork"]
+
+def get_account(index=None, id=None):
+    print(config["wallets"]["from_key"])
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    # if network.show_active() in config["networks"]:
+    return accounts.add(config["wallets"]["from_key"])
+    # return None
