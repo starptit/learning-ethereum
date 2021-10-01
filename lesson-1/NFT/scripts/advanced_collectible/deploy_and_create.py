cf5ea2b67e05ab1dd33c93415c7ecef35fe97eac
Shaw Vu <genesis@Shaws-iMac.local>
2021-10-01 12:02:10 +0700

    Add advanced script

---
+++ lesson-1/NFT/scripts/advanced_collectible/deploy_and_create.py
@@ -0,0 +1,28 @@
+from scripts.helpful_scripts import get_account, OPENSEA_URL, get_contract
+from brownie import AdvancedCollectible, network, config
+
+# sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+# OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+
+def deploy_and_create():
+    account = get_account()
+
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account}
+    )
+
+
+    tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
+    tx.wait(1)
+    print(
+        f"Awesome, you can view your NFT at {OPENSEA_URL.format(simple_collectible.address,simple_collectible.tokenCounter() - 1)}"
+        )
+    print("PLease wait up to 20 minutes, and hit the refresh metadata button")
+    return simple_collectible
+
+def main():
+    deploy_and_create()
--- lesson-1/NFT/scripts/helpful_scripts.py
+++ lesson-1/NFT/scripts/helpful_scripts.py
@@ -1,6 +1,13 @@
-from brownie import accounts, network, config, SimpleCollectible
+from brownie import accounts, network, config, LinkToken, MockV3Aggregator, MockOracle, VRFCoordinatorMock, Contract
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache", "mainnet-fork", "binance-fork", "matic-fork"]
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+contract_to_mock = {
+    "link_token": LinkToken,
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "oracle": MockOracle,
+}
 
 def get_account(index=None, id=None):
     print(config["wallets"]["from_key"])
@@ -12,4 +19,63 @@ def get_account(index=None, id=None):
         return accounts.load(id)
     # if network.show_active() in config["networks"]:
     return accounts.add(config["wallets"]["from_key"])
-    # return None
+    # return Nonesdffsdfsd
+
+def get_contract(contract_name):
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type.name, contract_address, contract_type.abi
+        )
+    return contract
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks...")
+    account = get_account()
+    print("Deploying Mock LinkToken...")
+    link_token = LinkToken.deploy({"from": account})
+    print(f"Deploying Mock VRF Coordinator...")
+    vrf_coordinator = VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print(f"VRFCoordinator deployed to {vrf_coordinator.address}")
+
+# def get_contract(contract_name):
+#     """If you want to use this function, go to the brownie config and add a new entry for
+#     the contract that you want to be able to 'get'. Then add an entry in the in the variable 'contract_to_mock'.
+#     You'll see examples like the 'link_token'.
+#         This script will then either:
+#             - Get a address from the config
+#             - Or deploy a mock to use for a network that doesn't have it
+#         Args:
+#             contract_name (string): This is the name that is refered to in the
+#             brownie config and 'contract_to_mock' variable.
+#         Returns:
+#             brownie.network.contract.ProjectContract: The most recently deployed
+#             Contract of the type specificed by the dictonary. This could be either
+#             a mock or the 'real' contract on a live network.
+#     """
+#     contract_type = contract_to_mock[contract_name]
+#     if network.show_active() in NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+#         if len(contract_type) <= 0:
+#             deploy_mocks()
+#         contract = contract_type[-1]
+#     else:
+#         try:
+#             contract_address = config["networks"][network.show_active()][contract_name]
+#             contract = Contract.from_abi(
+#                 contract_type._name, contract_address, contract_type.abi
+#             )
+#         except KeyError:
+#             print(
+#                 f"{network.show_active()} address not found, perhaps you should add it to the config or deploy mocks?"
+#             )
+#             print(
+#                 f"brownie run scripts/deploy_mocks.py --network {network.show_active()}"
+#             )
+#     return contract
+
